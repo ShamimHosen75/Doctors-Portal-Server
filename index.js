3ced45b41a2a330a61601efd3456574c700e1e97
ShamimHosen75 <developer.shamimhosen@gmail.com>
2023-01-24 00:05:21 +0600

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fllhx.mongodb.net/?retryWrites=true&w=majority`;
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.hdisf6s.mongodb.net/?retryWrites=true&w=majority`;
+
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -25,8 +27,6 @@ async function run(){
             const services = await cursor.toArray();
             res.send(services);
         })
-
-
     }
     finally{
 
@@ -34,6 +34,7 @@ async function run(){
 }
 
 run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
   res.send('Hello World Doctors!')
